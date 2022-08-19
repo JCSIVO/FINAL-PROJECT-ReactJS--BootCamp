03977d77fa8a1b02523e80f761366a3b014989b9
JCSIVO <josecsivo@gmail.com>
2022-08-19 13:19:22 +0200

    Final Projects

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Función anónima para crear un Componente principal
+ * @returns {React.Component} Componente principal de nuestra aplicación
+ */
+const App = () => {
+    return (
+      <div>
+        <hi>Proyecto Final React JS</hi>
+      </div>
+    );
+};
+
+export default App;
